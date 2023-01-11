14c6bbc089c5ee687740bdeb559c3259a9635d49
Juliet Lord <jlord24@students.polytechnic.org>
2023-01-11 14:54:02 -0800

    built constructor

--- Playlist.java
+++ Playlist.java
@@ -17,6 +17,10 @@ public class Playlist {
       * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
       * then use additional methods to add Songs in one-by-one
       */
+      public Playlist(){
+        totalDuration = 0;
+        playlist = new ArraryList<Song>();
+      }
 
 
       
